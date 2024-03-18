a6e73606ba9291735b27bf3ee9d966f29bef1959
CheolYeon <whcjdfus4831@naver.com>
2024-03-18 11:30:54 +0900

    TEST

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1710728175',resources: [
+    cacheName: 'chirpy-1710729017',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
